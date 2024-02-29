fa954cd679f217c683f72cdb8ab70a0711945cf0
Iván Parada <ivan.parada2001@alumnos.ubiobio.cl>
2024-02-29 18:11:57 -0300

    eventos del calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -3,14 +3,16 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { addHours } from 'date-fns';
 
-import { Navbar } from '../';
+import { CalendarEvent, Navbar } from '../';
 import { getMessagesES, localizer } from '../../helpers';
+import { useState } from 'react';
 
 
 export const CalendarPage = () => {
 
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week')
+
   const eventStyleGetter = ( event, start, end, isSelected) => {
-    console.log({event, start, end, isSelected})
 
     const style = {
       backgroundColor: '#347CF7',
@@ -24,12 +26,25 @@ export const CalendarPage = () => {
     }
   }
 
+  const onDoubleClick = (event) => {
+    console.log({doubleClick: event})
+  }
+
+  const onSelect = (event) => {
+    console.log({click: event})
+  }
+
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event);
+    setLastView( event )
+  }
+
 
   const events = [{
     title: 'Cumpleaños x',
     notes: 'Hay que comprar el pastel',
     start: new Date(),
-    end: addHours(new Date(), 1),
+    end: addHours(new Date(), 0.1),
     bgColor: '#fafafa',
     user: {
       _id: '123',
@@ -45,11 +60,18 @@ export const CalendarPage = () => {
       culture='es'
         localizer={localizer}
         events={events}
+        defaultView={lastView}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc( 100vh - 80px)' }}
         messages={getMessagesES()}
         eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent
+        }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
       />
     </>
   )

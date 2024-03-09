88b67c787328188466da36e0e6a82b9d101b1c35
Iván Parada <ivan.parada2001@alumnos.ubiobio.cl>
2024-03-09 20:00:59 -0300

    mantener estado de auth

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -13,4 +13,13 @@ const calendarApi = axios.create({
 
 //TODO: configurar interceptores
 
+calendarApi.interceptors.request.use(config => {
+
+    config.headers = {
+        'x-token': localStorage.getItem('token')
+    }
+
+    return config;
+})
+
 export default calendarApi;

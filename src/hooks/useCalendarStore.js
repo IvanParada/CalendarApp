285e7cc37549dc761b38ec5026f784d3d668dc0d
Iván Parada <ivan.parada2001@alumnos.ubiobio.cl>
2024-03-09 23:29:37 -0300

    actualizar evento

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { OnUpdateEvent, onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent } from "../store";
 import { calendarApi } from "../api";
 import { convertEventsToDateEvents } from "../helpers";
+import Swal from "sweetalert2";
 
 export const useCalendarStore = () => {
 
@@ -14,16 +15,25 @@ export const useCalendarStore = () => {
     }
 
     const startSavingEvent = async (calendarEvent) => {
-        //TODO: Update event
-        if (calendarEvent._id) {
-            //Actualizando
-            dispatch(OnUpdateEvent({ ...calendarEvent }))
 
-        } else {
+        try {
+
+            if (calendarEvent.id) {
+                //Actualizando
+                await calendarApi.put(`/events/${calendarEvent.id}`, calendarEvent);
+                dispatch(OnUpdateEvent({ ...calendarEvent, user }));
+                return;
+
+            }
             //Creando
             const { data } = await calendarApi.post('/events', calendarEvent);
             dispatch(onAddNewEvent({ ...calendarEvent, id: data.evento.id, user }))
+
+        } catch (error) {
+            Swal.fire('Error al guardar', error.response.data.msg, 'error')
+
         }
+
     }
 
     const startDeletingEvent = () => {
